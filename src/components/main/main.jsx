9c7c3097d419249875750fadc3264e70e30dd525
iewher <goshagang@mail.ru>
2023-08-13 20:53:07 +0500

    finished main logic

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,23 +4,43 @@ import "../../scss/main/main.scss";
 import Asteroids from "../asteroids/asteroids";
 
 export default function Main() {
-  const [count, setCount] = useState(0);
+  const [active, setActive] = useState(false);
+  const [basketValue, setBasketValue] = useState(0)
+
+  const handleSetActive = (value) => {
+    if (value !== active) {
+      setActive(value);
+    }
+  };
+
+  // console.log(active);
 
   return (
     <div className="main">
       <div className="container">
         <div className="container__asteroids">
           <h1>Ближайшие подлёты астероидов</h1>
-          <p>в киллометрах | в лунных орбитах</p>
-          <Asteroids />
-          <Asteroids />
-          <Asteroids />
-          <Asteroids />
+          <div className="choice">
+            <p
+              className={!active ? "active" : ""}
+              onClick={() => handleSetActive(false)}
+            >
+              в киллометрах
+            </p>
+            <span>|</span>
+            <p
+              className={active ? "active" : ""}
+              onClick={() => handleSetActive(true)}
+            >
+              в лунных орбитах
+            </p>
+          </div>
+          <Asteroids active={active}/>
         </div>
         <div className="container__basket">
           <div className="container__basket-text">
             <h1>Корзина</h1>
-            <p>2 астероида</p>
+            <p>{basketValue} астероидов</p>
           </div>
           <div className="container__basket-button">
             <button>Отправить</button>
